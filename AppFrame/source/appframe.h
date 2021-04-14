cd80cbae448c4f4b9bf9a532c0f1dbb7c79e895f
松尾忠彦 <mco.tdpk652227@outlook.com>
2021-04-15 01:55:17 +0900

    Picojsonファイルをライブラリ側に変更

--- AppFrame/source/appframe.h
+++ AppFrame/source/appframe.h
@@ -16,6 +16,7 @@
 #include "DxLib.h"
 #include "Application/ApplicationBase.h"
 #include "CFile/CFile.h"
+#include "Picojson/Picojson.h"
 #include "ResourceServer/ResourceServer.h"
 #include "Utility/UtilMacro.h"
 #include "Utility/Utility.h"
